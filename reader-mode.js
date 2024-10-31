41e8812df55dfc9d4aadc97428587be2f664df6b
Diego Comfort <diegocomfort@gmail.com>
2024-10-31 10:51:44 -0700

    Updated for sites with margins on the body
    Love you, GCC

--- reader-mode.js
+++ reader-mode.js
@@ -1,34 +1,41 @@
 function readerMode() {
-    // staitc bool active = false;
+    // Initialization
     if (typeof this.active === "undefined") {
 	this.active = false;
+	this.oldMargin = "";
+	this.oldPadding = "";
+	this.oldWidth = "";
+
+	// Just readjust things on resize
+	window.addEventListener("resize", () => {
+	    document.body.style.margin = "0% 0% 0% 0%";
+	    document.body.style.width = "max(6.5in, 33.33vw)";
+	    let computedWidth = window.getComputedStyle(document.body).width;
+	    let padding = `calc(50vw - 0.5 * ${computedWidth})`;
+	    // padding: top right bottom left
+	    document.body.style.padding = `0% ${padding} 0% ${padding}`;
+	});
     }
 
-    if (active) {
+    if (this.active) {	// Restore everything
 	document.body.style.width = this.oldWidth;
 	document.body.style.padding = this.oldPadding;
+	document.body.style.margin = this.oldMargin;
 	this.active = false;
 	return;
     }
 
+    // Center everything
+    this.oldMargin = document.body.style.margin;
     this.oldWidth = document.body.style.width;
-    document.body.style.width = "max(6.5in, 33.33vw)";
     this.oldPadding = document.body.style.padding;
+    document.body.style.margin = "0% 0% 0% 0%";
+    document.body.style.width = "max(6.5in, 33.33vw)";
     let computedWidth = window.getComputedStyle(document.body).width;
+    let padding = `calc(50vw - 0.5 * ${computedWidth})`;
     // padding: top right bottom left
-    document.body.style.padding =
-	`0% 0% 0% calc(50vw - 0.5 * ${computedWidth})`;
-    this.active = true
+    document.body.style.padding = `0% ${padding} 0% ${padding}`;
+    this.active = true;
 }
 
 readerMode();
-
-// Just readjust things
-window.addEventListener("resize", () => {
-    this.oldWidth = document.body.style.width;
-    document.body.style.width = "max(6.5in, 33we.33vw)";
-    let computedWidth = window.getComputedStyle(document.body).width;
-    // padding: top right bottom left
-    document.body.style.padding =
-	`0% 0% 0% calc(50vw - 0.5 * ${computedWidth})`;
-});
